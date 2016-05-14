01bbec945c08366b17aafdd6d18105f6f1c94845
sensssz <hjmsens@gmail.com>
2016-05-14 15:08:37 -0400

    Add exp_dist_test

---
+++ client/exp_dist_test.cc
@@ -0,0 +1,35 @@
+//
+// Created by Jiamin Huang on 5/14/16.
+//
+
+#include "exponential_distribution.h"
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::unordered_map;
+
+int main(int argc, char *argv[]) {
+    int lambda = 5;
+    if (argc == 2) {
+        lambda = atoi(argv[1]);
+    }
+    unordered_map<uint64_t, uint64_t> map;
+    exponential_distribution distribution(lambda, 100);
+
+    for (uint64_t count = 0; count < 10000; ++count) {
+        uint64_t val = distribution.next();
+        map[val]++;
+    }
+
+    for (int count = 0; count < 100; ++count) {
+        string val(map[count], '*');
+        cout << count << ": " << val << endl;
+    }
+
+    return 0;
+}
